8055ee52e97d327b035ec0161b1212361d0c8f67
Ventsislav Peychev <del.busque@gmail.com>
2025-01-04 22:59:13 +0200

    Init project

---
+++ 01-starting-project/app/awesome/page.js
@@ -0,0 +1,11 @@
+const awesome = () => {
+    return (
+        <main>
+            <h3>
+                This page is awesome!
+            </h3>
+        </main>
+    )
+}
+
+export default awesome;
