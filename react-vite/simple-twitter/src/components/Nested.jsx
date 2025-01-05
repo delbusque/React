115e8389965a9ae80526e9e7c43d6e4a142bc1c1
Ventsislav Peychev <del.busque@gmail.com>
2025-01-05 10:04:38 +0200

    Add components

---
+++ react-vite/simple-twitter/src/components/Nested.jsx
@@ -0,0 +1,10 @@
+const Nested = () => {
+    return (
+        <>
+            <h3>Max</h3>
+            <p>Js is awesome too !</p>
+        </>
+    )
+}
+
+export default Nested
---
+++ react-vite/simple-twitter/src/components/Post.jsx
@@ -0,0 +1,13 @@
+import Nested from "./Nested"
+
+const Post = () => {
+    return (
+        <main>
+            <h3>Delbusque</h3>
+            <p>Next.js is awesome !</p>
+            <Nested />
+        </main>
+    )
+}
+
+export default Post
