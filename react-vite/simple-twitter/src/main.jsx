17a8f3aa10a402661395bc2ebda9ed0198077d5a
delbusque <del.busque@gmail.com>
2025-01-09 09:57:51 +0200

    Add React Router

--- react-vite/simple-twitter/src/main.jsx
+++ react-vite/simple-twitter/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import RootLayout from "./routes/RootLayout.jsx";
+import Posts from "./routes/Posts.jsx";
+import NewPost from "./routes/NewPost.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Posts /> },
+      { path: "/create-post", element: <NewPost /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
