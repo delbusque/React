ffa7dca341ed9c9ad6e207b9db0a79174c1e41b9
Ventsislav Peychev <del.busque@gmail.com>
2025-03-26 11:20:14 +0200

    Add form state via useActionState hook

--- form-actions/src/components/NewOpinion.jsx
+++ form-actions/src/components/NewOpinion.jsx
@@ -1,23 +1,83 @@
+import { useActionState } from 'react';
+
 export function NewOpinion() {
+  const loadOpinionActions = (prevState, formData) => {
+    const userName = formData.get('userName');
+    const title = formData.get('title');
+    const body = formData.get('body');
+
+    let errors = [];
+
+    if (!userName.trim()) {
+      errors.push('Please fill your name!');
+    }
+    if (title.trim().length < 4) {
+      errors.push('Opinion title should be more than 3 characters long!');
+    }
+    if (body.trim().length < 4 || body.length > 300) {
+      errors.push('Your opinion should be between 4 and 300 characters long!');
+    }
+
+    if (errors.length > 0) {
+      return {
+        errors,
+        enteredValues: {
+          userName,
+          title,
+          body,
+        },
+      };
+    }
+
+    return { errors: null };
+  };
+
+  const [formState, formAction] = useActionState(loadOpinionActions, {
+    errors: null,
+  });
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" />
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              defaultValue={formState.enteredValues?.userName}
+            />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              defaultValue={formState.enteredValues?.title}
+            />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5}></textarea>
+          <textarea
+            id="body"
+            name="body"
+            rows={5}
+            defaultValue={formState.enteredValues?.body}
+          ></textarea>
         </p>
+
+        {formState.errors && (
+          <ul className="errors">
+            {formState.errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
 
         <p className="actions">
           <button type="submit">Submit</button>
